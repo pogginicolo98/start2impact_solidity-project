90bfd9babd943678ed5af55147b5998e33b502f0
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-12-14 22:45:27 +0100

    Setup

--- smart-contracts/test/TreasureNFT.js
+++ smart-contracts/test/TreasureNFT.js
@@ -42,44 +42,40 @@ describe("TreasureNFT contract", function () {
             expect(await contract.balanceOf(addr2.address)).to.equal(1);
         });
 
-    // it("Should fail if sender doesn’t have permission", async function () {
-    //   const initialOwnerBalance = await contract.balanceOf(owner.address);
-    //   const amount = ethers.utils.parseUnits("1", 18);
-    //
-    //   await expect(
-    //     contract.connect(addr1).transfer(owner.address, amount)
-    //   ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
-    //
-    //   expect(await contract.balanceOf(owner.address)).to.equal(initialOwnerBalance);
-    // });
-    //
-    // it("Should update balances after transfers", async function () {
-    //   const initialOwnerBalance = await contract.balanceOf(owner.address);
-    //   const amount1 = ethers.utils.parseUnits("50", 18);
-    //   const amount2 = ethers.utils.parseUnits("25", 18);
-    //
-    //   await contract.transfer(addr1.address, amount1);
-    //   await contract.transfer(addr2.address, amount2);
-    //
-    //   const finalOwnerBalance = await contract.balanceOf(owner.address);
-    //   expect(finalOwnerBalance).to.equal(initialOwnerBalance.sub(amount1.add(amount2)));
-    //
-    //   const addr1Balance = await contract.balanceOf(addr1.address);
-    //   expect(addr1Balance).to.equal(amount1);
-    //
-    //   const addr2Balance = await contract.balanceOf(addr2.address);
-    //   expect(addr2Balance).to.equal(amount2);
-    // });
-    //
-    // it("Should give permission to another account", async function () {
-    //   const initialOwnerBalance = await contract.balanceOf(owner.address);
-    //   const amount = ethers.utils.parseUnits("1", 18);
-    //
-    //   await expect(
-    //     contract.connect(addr1).transfer(owner.address, amount)
-    //   ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
-    //
-    //   expect(await contract.balanceOf(owner.address)).to.equal(initialOwnerBalance);
-    // });
+        it("Should fail if sender doesn’t have permission", async function () {
+            await contract.mintTresure(owner.address, "Token URI");
+            let tokenId = await contract.tokenOfOwnerByIndex(owner.address, 0);
+
+            await expect(
+                contract.connect(addr1).transferFrom(owner.address, addr1.address, tokenId)
+            ).to.be.revertedWith("ERC721: transfer caller is not owner nor approved");
+            expect(await contract.balanceOf(owner.address)).to.equal(1);
+        });
+
+        it("Should update balances after transfers", async function () {
+            await contract.mintTresure(owner.address, "Token URI1");
+            await contract.mintTresure(owner.address, "Token URI2");
+            let tokenId = await contract.tokenOfOwnerByIndex(owner.address, 0);
+            expect(await contract.balanceOf(owner.address)).to.equal(2);
+
+            await contract.transferFrom(owner.address, addr1.address, tokenId);
+            expect(await contract.balanceOf(owner.address)).to.equal(1);
+            expect(await contract.balanceOf(addr1.address)).to.equal(1);
+
+            await contract.connect(addr1).transferFrom(addr1.address, addr2.address, tokenId);
+            expect(await contract.balanceOf(addr1.address)).to.equal(0);
+            expect(await contract.balanceOf(addr2.address)).to.equal(1);
+        });
+
+        it("Should give permission to another account", async function () {
+            const initialOwnerBalance = await contract.balanceOf(owner.address);
+            const amount = ethers.utils.parseUnits("1", 18);
+
+            await expect(
+            contract.connect(addr1).transfer(owner.address, amount)
+            ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+
+            expect(await contract.balanceOf(owner.address)).to.equal(initialOwnerBalance);
+        });
     });
 });
